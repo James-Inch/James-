29c71e7642dfaaeb6ce5ffe5676404c1b41c1822
james inch <james.inch0103@gmail.com>
2019-04-26 22:53:01 -0400

    add js for nav dropdown

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 document.addEventListener('click', function (event) {
 
     // If the clicked element doesn't have the right selector, bail
-    if (event.target.matches('.hidden')) return; 
+    if (event.target.matches('.hidden')) 
+    
+    return; 
     
     // Don't follow the link 
     event.preventDefault();
     
     // Log the cicked element in thje conlsole
     console.log(event.target);
-}, false);
+    }, 
+false);
