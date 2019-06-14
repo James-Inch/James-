b6f6ff9805ff9ff4c1790b97d3e2f777c9ae690d
james inch <james.inch0103@gmail.com>
2019-06-14 13:00:55 -0400

    add margin to menu li

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-document.getElementById('menu').addEventListener('click', showMenu);
+window.onload = function() {
+    document.getElementById('menu').addEventListener('click', showMenu);
 
-function showMenu() {
-    var navMenu = document.getElementById('hidden');
-    navMenu.classList.remove('hidden');
-};
-
-document.getElementById('closeMenuBtn').addEventListener('click', closeMenu);
-
-function closeMenu() {
-    var navMenu = document.getElementById('hidden');
-    navMenu.classList.add('hidden');
-};
-
-document.getElementById('logo').addEventListener('click', goHome);
-
-function goHome() { 
-    window.location.assign('/portfolio.html');
-};
-
-var bio = document.getElementById('bioBtn');
-
-if (bio) {
-    bio.addEventListener('click', expandBio);
-    function expandBio() {
-        var expandedBioSection =  document.getElementById('hiddenBio');
-        expandedBioSection.classList.toggle('hiddenBio');
+    function showMenu() {
+        var navMenu = document.getElementById('hidden');
+        navMenu.classList.remove('hidden');
+    };
+    
+    document.getElementById('closeMenuBtn').addEventListener('click', closeMenu);
+    
+    function closeMenu() {
+        var navMenu = document.getElementById('hidden');
+        navMenu.classList.add('hidden');
+    };
+    
+    document.getElementById('logo').addEventListener('click', goHome);
+    
+    function goHome() { 
+        window.location.assign('/portfolio.html');
+    };
+    
+    var bio = document.getElementById('bioBtn');
+    
+    if (bio) {
+        bio.addEventListener('click', expandBio);
+        function expandBio() {
+            var expandedBioSection =  document.getElementById('hiddenBio');
+            expandedBioSection.classList.toggle('hiddenBio');
+        }
     }
 }
-
